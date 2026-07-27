fd908d2be02b9303af69839f338c502fed5c5192
Abiy Hailu <abiyhailu120@gmail.com>
2026-07-27 23:26:13 +0300

    feat:final-version

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig(({ mode }) => {
 	const env = loadEnv(mode, process.cwd(), "");
-	const odlTarget = env.VITE_ODL_HOST || "http://172.27.176.1:8181";
+	const odlTarget = env.VITE_ODL_HOST || "https://localhost:8443";
 
 	return {
 		plugins: [react(), tailwindcss()],
